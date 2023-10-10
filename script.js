20a015872aa980703015865d5ec64973bb894567
Obstagoon2 <121466657+Obstagoon2@users.noreply.github.com>
2023-10-10 12:54:48 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// Dark mode toggle button
+const toggleButton = document.getElementById('toggle-mode');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+    if (body.classList.contains('light-mode')) {
+        toggleButton.textContent = 'Light Mode';
+    } else {
+        toggleButton.textContent = 'Dark Mode';
+    }
+});
+
+// Note-saving and downloading
+const noteInput = document.getElementById('note-input');
+const saveButton = document.getElementById('save-note');
+const downloadLink = document.getElementById('download-link');
+
+saveButton.addEventListener('click', () => {
+    const noteText = noteInput.value;
+    const blob = new Blob([noteText], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    downloadLink.href = url;
+    downloadLink.style.display = 'block';
+    downloadLink.textContent = 'Download Note';
+    downloadLink.download = 'note.txt';
+});
