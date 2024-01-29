26e77e419491c75230de15c73e2dc7df093c2e4c
Obstagoon2 <121466657+Obstagoon2@users.noreply.github.com>
2024-01-29 13:43:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const body = document.body;
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('light-mode');
     if (body.classList.contains('light-mode')) {
-        toggleButton.textContent = 'Light Mode';
-    } else {
         toggleButton.textContent = 'Dark Mode';
+    } else {
+        toggleButton.textContent = 'Light Mode';
     }
 });
 
